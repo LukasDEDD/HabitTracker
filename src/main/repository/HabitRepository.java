68b8ea8bf0519f84daedc39434fb3b88288f4e7c
LukasDD82 <lukas.simek.de@gmail.com>
2026-02-09 18:43:15 +0100

    Initial commit + Model & Repository I done

--- src/main/repository/HabitRepository.java
+++ src/main/repository/HabitRepository.java
@@ -1,4 +1,3 @@
 package main.repository;
 
-public class HabitRepository {
 }
--- src/main/service/HabitService.java
+++ src/main/service/HabitService.java
@@ -1,15 +1,7 @@
 package main.service;
 
-import main.model.Habit;
 
-import java.util.List;
 
-public interface HabitService {
 
-    List<Habit> findAll();
-
-    Habit findById(int id);
-
-    Habit add(List<Habit>records);
 
 }
